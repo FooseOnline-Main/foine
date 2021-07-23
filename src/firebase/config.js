51c05b535a4e60cb62b6522983e829ec0c0532f5
Kenny Yankz <46938959+ToppYankah@users.noreply.github.com>
2021-07-23 13:56:27 +0000

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@
 // Contains the config details of firebase, this should NOT be committed to your repo
 // if the repo is public
 // You could optionally use .env file to store these data
+
 const value = process.env.REACT_APP_FIREBASE_CONFIG;
 const configuration = JSON.parse(value.toString());
 
